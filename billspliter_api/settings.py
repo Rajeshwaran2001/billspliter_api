0c97257b1d217044014fbf23fd945f5df37a394b
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2022-08-28 22:19:42 +0530

    main branch restored

--- billspliter_api/settings.py
+++ billspliter_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i8qqjcnr3@&p-53-g^6frywnn-*1!4jpp@)vrwntzsq$c8-9!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bill-spliter.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,23 +79,11 @@ WSGI_APPLICATION = 'billspliter_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'dfb3ovcbjq76ju',
-
-       'USER': 'bcafqdaedhawko',
-
-        'PASSWORD': 'e92c0b19c9a668155486bc7d56e31ff2233856104749ce427391c9eba572308a',
-
-         'HOST': 'ec2-34-227-135-211.compute-1.amazonaws.com',
-
-        'PORT': '5432',
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
- }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

f361c7a0f1ed213b38ca4d35710b8259df225bfb
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2022-09-04 00:32:07 +0530

    expense app created

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', home, name='api.home'),
     path('user/', include('api.user.urls')),
     path('groups/', include('api.groups.urls')),
+    path('expense/', include('api.expense.urls')),
     path('api-token-auth/', views.obtain_auth_token, name='api_token_auth'),
 ]

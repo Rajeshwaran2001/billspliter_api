d6d60d41dd0fc32670388d4424a38455bc79a705
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2022-09-04 18:22:03 +0530

    serializer update

--- api/expense/serializers.py
+++ api/expense/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 
-from .models import Expense
+from .models import Expense, Debt, ExpenseUser, Ex
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Expense
-        fields = ('name', 'expense_group', 'description', 'payment', 'amount','date','repayments','users','transaction_id')
+        fields = ('group_id', 'description', 'payment',
+                  'date', 'friendship_id', 'repayments',
+                  'user', 'transaction_id')
+
+
+class DebtSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Debt
+        fields = ('id', 'from_user', 'to_user', 'amount')
+
+
+class ExpenseUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExpenseUser
+        fields = ('id', 'paid_share', 'owed_share', 'net_balance')
